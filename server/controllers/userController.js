9d618c08334bb0271e9746300945b7e00e634b46
thisaldil <tdimith10@gmail.com>
2025-06-28 12:50:49 +0530

    Revert "Update userController.js"
    This reverts commit 6d316ad8eb66539be09007c9d3da93521394fa5e.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,17 +6,16 @@ const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 const nodemailer = require("nodemailer");
+const Invoice = require("../models/Invoice");
 
 //get user details
 exports.getUserDetails = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.userId).select(
-      "-password -__v"
-    );
-    if (!user) return res.status(404).json({ error: "User not found" });
-    res.json(user);
-  } catch (err) {
-    console.error("Error fetching user details:", err);
-    res.status(500).json({ error: "Internal server error" });
-  }
+    try {
+        const user = await User.findById(req.params.userId).select("-password -__v");
+        if (!user) return res.status(404).json({ error: "User not found" });
+        res.json(user);
+    } catch (err) {
+        console.error("Error fetching user details:", err);
+        res.status(500).json({ error: "Internal server error" });
+    }
 };
