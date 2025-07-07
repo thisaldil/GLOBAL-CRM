453878a7248e823db2a782c872a83768d6c55825
thisaldil <tdimith10@gmail.com>
2025-07-07 14:05:17 +0530

    Update send-holiday-emails.js

--- server/api/cron/send-holiday-emails.js
+++ server/api/cron/send-holiday-emails.js
@@ -42,9 +42,9 @@ module.exports = async (req, res) => {
 
     for (const customer of customers) {
       const personalizedBody = template.body
-        .replace(/{{fullName}}/g, customer.fullName || "Valued Customer")
-        .replace(/{{company}}/g, customer.company || "Your Company")
-        .replace(/{{date}}/g, todayFormatted);
+        .replace(/{{\s*fullName\s*}}/g, customer.fullName || "Valued Customer")
+        .replace(/{{\s*company\s*}}/g, customer.company || "Your Company")
+        .replace(/{{\s*date\s*}}/g, todayFormatted);
 
       await sendEmail({
         to: customer.email,
