a84ddd183e4c612e02148aceeb7e4214217384e0
thisaldil <tdimith10@gmail.com>
2025-07-07 15:26:55 +0530

    Revert "Update emailController.js"
    This reverts commit 70a54f80a123da3894b4db07e75e923a1c5d3010.

--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -10,19 +10,13 @@ exports.sendBulkEmail = async (req, res) => {
     if (!template)
       return res.status(404).json({ message: "Template not found" });
 
-    const todayFormatted = new Date().toLocaleDateString("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    });
-
     const customers = await Customer.find({ email: { $exists: true } });
 
     for (const customer of customers) {
       const personalizedBody = template.body
-        .replace(/{{\s*fullName\s*}}/g, customer.fullName || "Valued Customer")
-        .replace(/{{\s*company\s*}}/g, customer.company || "Your Company")
-        .replace(/{{\s*date\s*}}/g, todayFormatted);
+        .replace(/{{fullName}}/g, customer.fullName || "Valued Customer")
+        .replace(/{{company}}/g, customer.company || "Your Company")
+        .replace(/{{date}}/g, todayFormatted);
 
       await sendEmail({
         to: customer.email,
