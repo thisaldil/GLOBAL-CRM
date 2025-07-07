3a3a3bfea8a58283aae2e3a9fec6c2e8b1dbedae
thisaldil <tdimith10@gmail.com>
2025-07-07 10:58:45 +0530

    cc

--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -1,6 +1,6 @@
 const Customer = require("../models/customer");
 const EmailTemplate = require("../models/emailTemplate");
-const nodemailer = require("nodemailer");
+const sendEmail = require("../utils/sendEmail");
 
 exports.sendBulkEmail = async (req, res) => {
   const { templateId } = req.body;
@@ -12,31 +12,22 @@ exports.sendBulkEmail = async (req, res) => {
 
     const customers = await Customer.find({ email: { $exists: true } });
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER, // e.g., your@gmail.com
-        pass: process.env.EMAIL_PASS, // App Password or real password
-      },
-    });
-
     for (const customer of customers) {
       const personalizedBody = template.body.replace(
         /{{fullName}}/g,
-        customer.fullName
+        customer.fullName || "Valued Customer"
       );
 
-      await transporter.sendMail({
-        from: `"ABMSZ" <${process.env.EMAIL_USER}>`,
+      await sendEmail({
         to: customer.email,
         subject: template.subject,
         html: personalizedBody,
       });
     }
 
-    res
-      .status(200)
-      .json({ message: `Emails sent to ${customers.length} customers` });
+    res.status(200).json({
+      message: `Emails sent to ${customers.length} customers`,
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Failed to send emails", error: err });
